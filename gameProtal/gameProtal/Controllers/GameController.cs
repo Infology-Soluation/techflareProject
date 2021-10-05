28898af05788b1c7f4721ca5425da17571e5460f
dilum96 <tharakadilum5@gmail.com>
2021-10-06 00:21:19 +0530

    2nd commite

---
+++ gameProtal/gameProtal/Controllers/GameController.cs
@@ -0,0 +1,35 @@
+﻿using gameProtal.Models;
+using gameProtal.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gameProtal.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly GameRepository _gameRepository = null;
+
+        public GameController()
+        {
+            _gameRepository = new GameRepository();
+        }
+
+        public List<GameModel> GetAllGames()
+        {
+            return _gameRepository.GetAllGames(); 
+        }
+
+        public GameModel GetGame(int id)
+        {
+            return _gameRepository.GetGameById(id);
+        }
+
+        public List<GameModel> SerachGames(string title, string developer)
+        {
+            return _gameRepository.SearchGames(title, developer);
+        }
+    }
+}
---
+++ gameProtal/gameProtal/Repository/GameRepository.cs
@@ -0,0 +1,37 @@
+﻿using gameProtal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gameProtal.Repository
+{
+    public class GameRepository
+    {
+        public List<GameModel> GetAllGames()
+        {
+            return DataSource();
+        }
+
+        public GameModel GetGameById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<GameModel> SearchGames(string title, string developer)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.developer.Contains(developer)).ToList();
+        }
+
+        private List<GameModel> DataSource()
+        {
+            return new List<GameModel>()
+            { 
+                new GameModel(){Id=1, Title="BATTERFEILD", developer="sinec" },
+                new GameModel(){Id=2, Title="MVC", developer="sinec" },
+                new GameModel(){Id=3, Title="MMW", developer="sinec" },
+                new GameModel(){Id=4, Title="WWE", developer="sinec" },
+            };
+        }
+    }
+}
